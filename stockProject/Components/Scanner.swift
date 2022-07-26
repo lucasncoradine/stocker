4395e0f91259a7d19ab59897e4ffe1ff2a5cee66
Lucas <lucasncoradine@gmail.com>
2022-07-26 12:29:49 -0300

    bugfix - validando acesso à câmera

--- stockProject/Components/Scanner.swift
+++ stockProject/Components/Scanner.swift
@@ -16,7 +16,7 @@ struct QRCodeFrame: Shape {
         
         let origin = CGPoint(x: rect.midX - size.width / 2, y: rect.midY - size.height / 2)
         path.addRoundedRect(in: CGRect(origin: origin, size: size), cornerSize: .init(width: 26, height: 26))
-
+        
         return path
     }
 }
@@ -29,6 +29,7 @@ struct Scanner: View {
     @Environment(\.dismiss) var dismiss
     @State var showError: Bool = false
     @State var errorMessage: String = ""
+    @State var accessGranted: Bool
     
     init(completion: @escaping (_ result: ScanResult) -> Void,
          failure: @escaping FailureClosure,
@@ -37,49 +38,74 @@ struct Scanner: View {
         self.completion = completion
         self.failureClosure = failure
         self.dismissAfterResult = dismissAfterResult
+        self._accessGranted = State(initialValue: UIApplication.shared.isCameraAuthorized)
+    }
+    
+    private func openSettings() {
+        guard let settingsUrl: URL = URL(string: UIApplication.openSettingsURLString)
+        else { return }
+        
+        if UIApplication.shared.canOpenURL(settingsUrl) {
+            UIApplication.shared.open(settingsUrl)
+        }
     }
     
     var body: some View {
         ZStack {
-            ZStack {
-                CodeScannerView(codeTypes: [.qr],
-                                scanMode: .oncePerCode,
-                                showViewfinder: true) { result in
-                    switch result {
-                    case .success(let result):
-                        completion(result)
-                        
-                        if dismissAfterResult {
-                            dismiss()
+            if accessGranted {
+                ZStack {
+                    CodeScannerView(codeTypes: [.qr],
+                                    scanMode: .oncePerCode) { result in
+                        switch result {
+                        case .success(let result):
+                            completion(result)
+                            
+                            if dismissAfterResult {
+                                dismiss()
+                            }
+                        case .failure(let error):
+                            failureClosure(error.localizedDescription)
                         }
-                    case .failure(let error):
-                        failureClosure(error.localizedDescription)
+                    }
+                    
+                    Rectangle()
+                        .foregroundColor(Color.black.opacity(0.65))
+                        .mask {
+                            QRCodeFrame().fill(style: FillStyle(eoFill: true))
+                        }
+                }
+                .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Spacer()
+                    
+                    Text(Strings.scannerText)
+                        .foregroundColor(.white)
+                        .bold()
+                        .padding(.bottom, 60)
+                }
+            } else {
+                VStack(spacing: 30) {
+                    Image(systemName: "video.slash")
+                        .font(.system(size: 32))
+                    
+                    Text("O aplicativo não possui permissão de acesso à câmera!")
+                        .font(.headline)
+                        .multilineTextAlignment(.center)
+                    
+                    Button(action: openSettings) {
+                        Text("Permitir acesso")
                     }
                 }
-                
-                Rectangle()
-                    .foregroundColor(Color.black.opacity(0.65))
-                    .mask {
-                        QRCodeFrame().fill(style: FillStyle(eoFill: true))
-                    }
-            }
-            .edgesIgnoringSafeArea(.all)
-            
-            VStack {
-                Spacer()
-                
-                Text(Strings.scannerText)
-                    .foregroundColor(.white)
-                    .bold()
-                    .padding(.bottom, 60)
             }
         }
+        .navigationBarTitleDisplayMode(.inline)
         .navigationBarBackButtonHidden(true)
         .toolbar {
             ToolbarItemGroup(placement: .navigationBarLeading) {
                 Button(action: { dismiss() }) {
                     Image(systemName: "chevron.left")
-                        .foregroundColor(.white)
+                        .foregroundColor(accessGranted ? .white : .black)
                         .font(.system(size: 16, weight: .bold))
                 }
             }
@@ -91,6 +117,8 @@ struct Scanner: View {
 
 struct Scanner_Previews: PreviewProvider {
     static var previews: some View {
-        Scanner() { _ in } failure: { _ in }
+        NavigationView {
+            Scanner() { _ in } failure: { _ in }
+        }
     }
 }
---
+++ stockProject/Utils/Extensions/BindingExtension.swift
@@ -0,0 +1,16 @@
+//
+//  BindingExtension.swift
+//  stockProject
+//
+//  Created by Lucas Negreiros Coradine on 26/07/22.
+//
+
+import SwiftUI
+
+extension Binding where Value == Bool {
+    var not: Binding<Bool> {
+        return !self
+//        return !(self.wrappedValue)
+    }
+}
+
